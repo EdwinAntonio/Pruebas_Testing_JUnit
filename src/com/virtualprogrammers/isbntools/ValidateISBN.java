cdde402dea1a260ac839756fddcc6abc8cf035a4
EdwinAntonio <edwinam007@hotmail.com>
2021-11-02 15:35:16 -0700

    V0.0.2 - Only 10 Digits format

--- src/com/virtualprogrammers/isbntools/ValidateISBN.java
+++ src/com/virtualprogrammers/isbntools/ValidateISBN.java
@@ -2,9 +2,22 @@ package com.virtualprogrammers.isbntools;
 
 public class ValidateISBN {
     public boolean checkAValidateISBN(String isbn) {
+        if(isbn.length() < 10) throw new NumberFormatException("El ISBN debe de tener por lo menos 10 digitos");
+        if(isbn.length() > 10) throw new NumberFormatException("El ISBN debe de tener por lo menos 10 digitos");
         int total = 0;
         for (int i=0; i<10; i++){
-            total += isbn.charAt(i) * (10-i);
+            if(!Character.isDigit(isbn.charAt(i))) {
+                if(i == 9 && isbn.charAt(i) == 'X'){
+                    //this is ok np! :D
+                    total += 10;
+                }else {
+                    throw new NumberFormatException("El ISBN deben de ser puros numeros enteros");
+                }
+            }
+            total += Character.getNumericValue(isbn.charAt(i)) * (10-i); /*Cuando realicemos valores con ASCII siempre
+                                                                           tener en cuenta que hay que volver a
+                                                                           convertirlos en numericos, por eso el
+                                                                           Character.getNumberValue*/
         }
         if(total % 11 == 0){
             return true;
--- test/com/virtualprogrammers/isbntools/ExampleTest.java
+++ test/com/virtualprogrammers/isbntools/ExampleTest.java
@@ -22,4 +22,23 @@ public class ExampleTest {
         assertFalse(new ValidateISBN().checkAValidateISBN("0140449117"));
     }
 
+    @Test(expected = NumberFormatException.class)
+    public void nineNumbersISBNAreNotAllowed(){
+        new ValidateISBN().checkAValidateISBN("123456789");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void moreThanTenNumbersISBNAreNotAllowed(){
+        new ValidateISBN().checkAValidateISBN("12345678901");
+    }
+
+    @Test(expected = NumberFormatException.class)
+    public void noneNumericISBNAreNotAllowed(){
+        new ValidateISBN().checkAValidateISBN("helloworld");
+    }
+
+    @Test
+    public void isbnLastNumberIsAnX(){
+        assertTrue(new ValidateISBN().checkAValidateISBN("012000030X"));
+    }
 }
